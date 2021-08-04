94033c864995d3b5797abe1704c0023b864d7639
extranuts <QroWka8462>
2021-08-04 21:21:17 +0300

    Learning PrepareStatement

--- src/com/dmdev/jdbc/starter/JdbcRunner.java
+++ src/com/dmdev/jdbc/starter/JdbcRunner.java
@@ -2,33 +2,30 @@ package com.dmdev.jdbc.starter;
 
 import com.dmdev.jdbc.starter.util.ConnectionManager;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
 
-        String flightId = "2 OR 1 = 1; DROP TABLE info;"; //SQL Inject
+        Long flightId = 2L; //SQL Inject
         final var result = getTicketsByFlightId(flightId);
         System.out.println(result);
-
-
     }
-    private static List<Long> getTicketsByFlightId(String flightId) throws SQLException {
+
+    private static List<Long> getTicketsByFlightId(Long flightId) throws SQLException {
         String sql = """
                 SELECT id
                 FROM ticket
-                WHERE flight_id = %s
-                """.formatted(flightId);
+                WHERE flight_id = ? 
+                """;
         List<Long> result = new ArrayList<>();
         try (var connection = ConnectionManager.open();
-             final var statement = connection.createStatement()) {
-            final var resultSet = statement.executeQuery(sql);
-            while (resultSet.next()){
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, flightId);
+            final var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
                 result.add(resultSet.getObject("id", Long.class)); //Null Safety
             }
         }
