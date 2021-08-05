6ce9229c6fac55a6a9a084717120f8451a91114d
extranuts <QroWka8462>
2021-08-05 12:34:32 +0300

    JDBC Batch DDL issue

--- src/com/dmdev/jdbc/starter/TransactionRunner.java
+++ src/com/dmdev/jdbc/starter/TransactionRunner.java
@@ -3,36 +3,28 @@ package com.dmdev.jdbc.starter;
 import com.dmdev.jdbc.starter.util.ConnectionManager;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class TransactionRunner {
     public static void main(String[] args) throws SQLException {
         long flightId = 9;
-        var deleteFlightSql = "DELETE FROM flight WHERE id = ?";
-        var deleteTicketsSql = "DELETE FROM ticket WHERE flight_id = ?";
+        var deleteFlightSql = "DELETE FROM flight WHERE id = " + flightId;
+        var deleteTicketsSql = "DELETE FROM ticket WHERE flight_id = " + flightId;
         Connection connection = null;
-        PreparedStatement deleteFlightStatement = null;
-        PreparedStatement deleteTicketsStatement = null;
+
+        Statement statement = null;
+
         try {
             connection = ConnectionManager.open();
-            deleteFlightStatement = connection.prepareStatement(deleteFlightSql);
-<<<<<<< HEAD
-            deleteTicketsStatement = connection.prepareStatement(deleteTicketsSql);
-=======
-            deleteTicketsStatement = connection.prepareStatement(deleteTicketsSql)
->>>>>>> origin/master
-
             connection.setAutoCommit(false);
 
-            deleteFlightStatement.setLong(1, flightId);
-            deleteTicketsStatement.setLong(1, flightId);
+            statement = connection.createStatement();
+            statement.addBatch(deleteTicketsSql);
+            statement.addBatch(deleteFlightSql);
 
-            deleteTicketsStatement.executeUpdate();
-            if (true) {
-                throw new RuntimeException("Oops");
-            }
-            deleteFlightStatement.executeUpdate();
+            var ints = statement.executeBatch();
 
             connection.commit();
 
@@ -46,11 +38,8 @@ public class TransactionRunner {
             if (connection != null) {
                 connection.close();
             }
-            if (deleteFlightStatement != null) {
-                deleteFlightStatement.close();
-            }
-            if (deleteTicketsStatement != null) {
-                deleteFlightStatement.close();
+            if (statement != null) {
+                statement.close();
             }
         }
     }
